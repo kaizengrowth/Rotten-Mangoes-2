552c7420016d001aa6d0bbbac294f2e1f739a9d8
vagrant <vagrant@lighthouse-labs>
2016-07-18 22:35:11 +0000

    Commit 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
 
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
