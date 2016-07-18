e2a21982c90c5a3570b9dc2a130a4bdc7d662f26
vagrant <vagrant@lighthouse-labs>
2016-07-18 22:09:11 +0000

    up to 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  resources :movies
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
