2dd64143d93fbf8d6ac264efaa7d80dddd67951f
kaizencole <papertigerlily@gmail.com>
2016-07-20 22:12:30 +0000

    Admin Delete and Edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,4 +15,33 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user=User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user=User.find(params[:id])
+
+    if !params[:admin]
+      params[:admin] = false
+    end
+
+    if @user.update(params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
+
 end
